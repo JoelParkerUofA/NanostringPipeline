f2574500e53a39a6f1f22460d946a90de96b0224
JoelParkerUofA <81324411+JoelParkerUofA@users.noreply.github.com>
2024-01-10 09:44:23 -0700

    adding files

---
+++ Code/PackageLoading.R
@@ -0,0 +1,28 @@
+################################################################################
+## The purpose of this script is to load and install the required packages 
+## For the workflow
+################################################################################
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+# The following initializes most up to date version of Bioc
+#BiocManager::install(version="3.15")
+
+#BiocManager::install("NanoStringNCTools")
+#BiocManager::install("GeomxTools")
+#BiocManager::install("GeoMxWorkflows")
+
+library(NanoStringNCTools)
+library(GeomxTools)
+library(GeoMxWorkflows)
+library(knitr)
+library(ggplot2)
+library(ggforce)
+library(dplyr)
+library(scales) # for percent
+library(reshape2)  
+library(cowplot) 
+
+
+
---
+++ R/QC_histogram.R
@@ -0,0 +1,27 @@
+#
+# Creates a histogram function of all the QC parameters for each sample.
+# This function depends on the information from the sData function which is a
+# shortcut to the segment level metadata. The QC metrics are appended to the
+# metadata.
+#
+
+# 1 Graphical summaries of QC statistics plot function
+QC_histogram <- function(assay_data = NULL,
+                         annotation = NULL,
+                         fill_by = NULL,
+                         thr = NULL,
+                         scale_trans = NULL) {
+  plt <- ggplot(assay_data,
+                aes_string(x = paste0("unlist(`", annotation, "`)"),
+                           fill = fill_by)) +
+    geom_histogram(bins = 50) +
+    geom_vline(xintercept = thr, lty = "dashed", color = "black") +
+    theme_bw() + guides(fill = "none") +
+    facet_wrap(as.formula(paste("~", fill_by)), nrow = 4) +
+    labs(x = annotation, y = "Segments, #", title = annotation)
+  if(!is.null(scale_trans)) {
+    plt <- plt +
+      scale_x_continuous(trans = scale_trans)
+  }
+  plt
+}
